9e5dba7852b6d15ea16bf4800865fbe2d9060284
Benoit Pouzet <bpouzet@gmail.com>
2023-02-22 19:15:12 +1100

    chore(deps): bump deps version and fix entry point (#19)

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import { ExpoRoot } from 'expo-router' ;
-import { RequireContext } from 'expo-router/build/types' ;
 import { registerRootComponent } from 'expo' ;
 
 import AppRoot from '@components/AppRoot' ;
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
-  const ctx: RequireContext = require.context('./app') ;
+  const ctx = require.context('./app') ;
   return (
     <AppRoot>
       <ExpoRoot context={ ctx } />
