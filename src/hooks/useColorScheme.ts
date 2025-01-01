14b6fa21a25f5a741128018ae25619b71c20d8b5
Benoit Pouzet <bpouzet@gmail.com>
2025-01-01 18:05:52 +1100

    chore: improve slash screen and fix system theme

--- src/hooks/useColorScheme.ts
+++ src/hooks/useColorScheme.ts
@@ -1,20 +1,12 @@
-import { Appearance, useColorScheme as _useColorScheme } from 'react-native' ;
-import { useEffect } from 'react' ;
+import { useColorScheme as _useColorScheme } from 'react-native' ;
 import useSettingsStore from '@hooks/useSettingsStore' ;
 
 export default function useColorScheme() {
   const themeStore = useSettingsStore(
     (state) => state.theme
   ) ;
-  const systemTheme = _useColorScheme() || 'default' ;
 
-  useEffect(() => {
-    if (themeStore === 'light' || themeStore === 'dark') {
-      Appearance.setColorScheme(themeStore) ;
-    } else {
-      Appearance.setColorScheme(null) ;
-    }
-  }, [ themeStore ]) ;
+  const systemTheme = _useColorScheme() ;
 
   // Return the appropriate theme without causing side effects
   return themeStore === 'default' ? systemTheme : themeStore ;
